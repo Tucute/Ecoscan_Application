83f2f6cbdb84f794f715b433e6442e6f1d707247
ThangNH23 <huuthang533540403@gmail.com>
2024-03-21 00:22:43 +0700

    thang/viewAccount

---
+++ src/screen/EditProfiles.tsx
@@ -0,0 +1,204 @@
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+    Modal,
+    Alert,
+    TextInput,
+} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useQuery, useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+import useGetUser from '../hook/useGetUser';
+import Entypo from 'react-native-vector-icons/Entypo';
+import Zocial from 'react-native-vector-icons/Zocial';
+// import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+interface getProfile {
+    id: number;
+    email: string;
+    name: string;
+    avatar: string;
+    phone: string;
+}
+const EditProfiles = ({ navigation }: any) => {
+
+    const { user } = useGetUser();
+
+    return (
+        <View style={styles.profileContainer}>
+            <TouchableOpacity style={styles.iconBack} onPress={() => navigation.goBack()}>
+                <Image
+                    source={require('../assets/CompareInterface-icon/Iconback.png')}>
+                </Image>
+            </TouchableOpacity>
+            <View style={styles.headerProfile}>
+                <View style={styles.profileTitle}>
+                    <Text style={styles.profileText}>Account</Text>
+                </View>
+                <View style={styles.profileImage}>
+                    <Image
+                        style={styles.userProfileImage}
+                        source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-gcQt4cLfOtecrvODooKFD6cRWVUsX5yGjQ&usqp=CAU' }}
+                    />
+                    <TouchableOpacity>
+                        <Entypo style={{ marginLeft: 120, bottom: 20 }} name='camera' size={20} color='#32ff7e' />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.profileName}>
+                    <Text style={styles.textName}>{user?.name}</Text>
+                </View>
+                <View style={styles.profileEmail}>
+                    <Text style={styles.email}>{user?.email}</Text>
+                </View>
+            </View>
+
+            <View style={styles.componentInfo}>
+                <View style={styles.componentUserName}>
+                    <Entypo style={styles.userNameIcon} name='user' size={25} color={'#7052ff'} />
+                    <View style={styles.userNameGroup}>
+                        <TextInput style={{ color: '#C3C7C7' }}>{user?.name}</TextInput>
+                    </View>
+                </View>
+                <View style={styles.componentUserName}>
+                    <Zocial style={styles.userNameIcon} name="email" size={30} color="#B3CB1D"></Zocial>
+                    <View style={styles.userNameGroup}>
+                        <TextInput numberOfLines={1} style={{ color: '#C3C7C7', width: '90%' }}>{user?.email}</TextInput>
+                    </View>
+                </View>
+                <View style={styles.componentUserName}>
+                    <AntDesign style={styles.userNameIcon} name="phone" size={30} color="#B3CB1D" />
+                    <View style={styles.userNameGroup}>
+                        <TextInput style={{ color: '#C3C7C7' }}>08754566878</TextInput>
+                    </View>
+                </View>
+            </View>
+
+            <View style={styles.fcUpdate}>
+                <TouchableOpacity style={{ flexDirection: 'row', alignSelf: 'center' }}>
+                    <Text style={styles.textUpdate}>Sign out</Text>
+                    <FontAwesome style={{ alignSelf: 'center', marginLeft: 10 }} name='sign-out' size={30} />
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    profileContainer: {
+        flex: 1,
+        backgroundColor: '#333',
+    },
+    iconBack: {
+        marginHorizontal: 20,
+        marginTop: 20,
+        borderRadius: 15,
+    },
+    headerProfile: {
+        width: 200,
+        alignSelf: 'center',
+        position: 'absolute',
+        marginVertical: 30
+    },
+    profileText: {
+        color: "#fff",
+        fontSize: 18,
+        textAlign: 'center'
+    },
+    profileImage: {
+        marginVertical: 20,
+    },
+    userProfileImage: {
+        width: 120,
+        height: 120,
+        borderRadius: 100,
+        alignSelf: 'center'
+    },
+    profileName: {
+        alignItems: 'center'
+    },
+    profileEmail: {
+        alignItems: 'center',
+        marginVertical: 5
+    },
+    email: {
+        color: '#C3C7C7'
+    },
+    textName: {
+        color: '#fff',
+        fontSize: 22,
+        fontWeight: 'bold',
+    },
+    // componentInfo: {
+    //   top: 260,
+    // },
+    styleUser: {
+        borderRadius: 100,
+        width: 100,
+        alignSelf: 'center',
+    },
+    componentAccount: {
+        backgroundColor: '#3E3D3D',
+        flexDirection: 'row',
+        borderRadius: 15,
+        width: "85%",
+        height: 60,
+        shadowColor: 'rgba(0, 0, 0, 0.5)',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 1,
+        shadowRadius: 4,
+        elevation: 4,
+        marginVertical: 10,
+        justifyContent: 'space-evenly',
+        marginHorizontal: 30,
+    },
+    fcUpdate: {
+        top: 300,
+        borderRadius: 25,
+        alignSelf: 'center',
+        backgroundColor: '#fff',
+        width: '85%',
+    },
+    textUpdate: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        marginVertical: 15,
+        textAlign: 'center',
+    },
+    componentUserName: {
+        display: 'flex',
+        flexDirection: 'row',
+        margin: 10,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#BA8F8F'
+    },
+    componentInfo: {
+        display: 'flex',
+        flexDirection: 'column',
+        marginVertical: 350,
+        marginHorizontal: 20,
+        width: 350,
+        borderRadius: 10,
+    },
+    userNameIcon: {
+        alignSelf: 'center',
+        left: 15
+    },
+    userNameGroup: {
+        marginHorizontal: 25,
+        marginVertical: 10,
+        width: '90%'
+    },
+});
+
+export default EditProfiles;
--- src/screen/Profile.tsx
+++ src/screen/Profile.tsx
@@ -8,12 +8,16 @@ import {
   Alert,
   TextInput,
 } from 'react-native';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {useQuery, useMutation} from '@tanstack/react-query';
+import { useQuery, useMutation } from '@tanstack/react-query';
 import axios from 'axios';
-// import useUser from '../../hooks/useUser';
 import useGetUser from '../hook/useGetUser';
+import Entypo from 'react-native-vector-icons/Entypo';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Feather from 'react-native-vector-icons/Feather';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 interface getProfile {
   id: number;
   email: string;
@@ -21,77 +25,130 @@ interface getProfile {
   avatar: string;
   phone: string;
 }
-const Profile = ({navigation}: any) => {
-//   const [userData, setUserData] = useState<getProfile | undefined>();
-//   const [modalVisible, setModalVisible] = useState(false);
-//   const [newUser, setNewUser] = useState<getProfile>();
-//   const {user} = useGetUser();
-//   useEffect(() => {
-//     if (user) {
-//       setUserData(user);
-//     }
-//   }, [user]);
+const Profile = ({ navigation }: any) => {
 
-//   const mutation = useMutation({
-//     mutationFn: async (data: getProfile) => {
-//       try {
-//         const jsonValue = await AsyncStorage.getItem('user');
-//         const value = jsonValue != null ? JSON.parse(jsonValue) : null;
-//         const token = value.token;
-//         const response = await axios.post(
-//           `https://2cf2-14-176-231-248.ngrok-free.app/api/update-user/`,
-//           data,
-//           {
-//             headers: {
-//               'Content-Type': 'application/json',
-//               Authorization: `Bearer ${token}`,
-//             },
-//           },
-//         );
-//         if (response.status === 200) {
-//           Alert.alert('Success', 'Update successfully');
-//         } else {
-//           Alert.alert('Invalid information!');
-//         }
-//         return response.data;
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     },
-//   });
+  const { user } = useGetUser();
+  //   const [userData, setUserData] = useState<getProfile | undefined>();
+  //   const [modalVisible, setModalVisible] = useState(false);
+  //   const [newUser, setNewUser] = useState<getProfile>();
+  //   const {user} = useGetUser();
+  //   useEffect(() => {
+  //     if (user) {
+  //       setUserData(user);
+  //     }
+  //   }, [user]);
 
-//   const handleLogout = async () => {
-//     await AsyncStorage.removeItem('user');
-//     const user = await AsyncStorage.getItem('user');
-//     if (user === null) {
-//       navigation.navigate('LandingPage');
-//     }
-//   };
+  //   const mutation = useMutation({
+  //     mutationFn: async (data: getProfile) => {
+  //       try {
+  //         const jsonValue = await AsyncStorage.getItem('user');
+  //         const value = jsonValue != null ? JSON.parse(jsonValue) : null;
+  //         const token = value.token;
+  //         const response = await axios.post(
+  //           `https://2cf2-14-176-231-248.ngrok-free.app/api/update-user/`,
+  //           data,
+  //           {
+  //             headers: {
+  //               'Content-Type': 'application/json',
+  //               Authorization: `Bearer ${token}`,
+  //             },
+  //           },
+  //         );
+  //         if (response.status === 200) {
+  //           Alert.alert('Success', 'Update successfully');
+  //         } else {
+  //           Alert.alert('Invalid information!');
+  //         }
+  //         return response.data;
+  //       } catch (error) {
+  //         console.error(error);
+  //       }
+  //     },
+  //   });
 
-//   const handleOnChange = (key: string, value: string) => {
-//     setNewUser(prevUserData => ({
-//       ...prevUserData,
-//       [key]: value,
-//     }));
-//   };
-//   const handleEditProfile = async () => {
-//     try {
-//       if (data) {
-//         setNewUser({...data});
-//       }
-//       setModalVisible(true);
-//     } catch (e) {
-//       console.log('Error: ', e);
-//     }
-//   };
-//   const handleSaveProfile = () => {
-//     setUserData(newUser);
-//     mutation.mutate(newUser);
-//     setModalVisible(!modalVisible);
-//   };
+  //   const handleLogout = async () => {
+  //     await AsyncStorage.removeItem('user');
+  //     const user = await AsyncStorage.getItem('user');
+  //     if (user === null) {
+  //       navigation.navigate('LandingPage');
+  //     }
+  //   };
+
+  //   const handleOnChange = (key: string, value: string) => {
+  //     setNewUser(prevUserData => ({
+  //       ...prevUserData,
+  //       [key]: value,
+  //     }));
+  //   };
+  //   const handleEditProfile = async () => {
+  //     try {
+  //       if (data) {
+  //         setNewUser({...data});
+  //       }
+  //       setModalVisible(true);
+  //     } catch (e) {
+  //       console.log('Error: ', e);
+  //     }
+  //   };
+  //   const handleSaveProfile = () => {
+  //     setUserData(newUser);
+  //     mutation.mutate(newUser);
+  //     setModalVisible(!modalVisible);
+  //   };
   return (
     <View style={styles.profileContainer}>
-     
+      <TouchableOpacity style={styles.iconBack} onPress={() => navigation.goBack()}>
+        <Image
+          source={require('../assets/CompareInterface-icon/Iconback.png')}>
+        </Image>
+      </TouchableOpacity>
+      <View style={styles.headerProfile}>
+        <View style={styles.profileTitle}>
+          <Text style={styles.profileText}>Profile</Text>
+        </View>
+        <View style={styles.profileImage}>
+          <Image
+            style={styles.userProfileImage}
+            source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-gcQt4cLfOtecrvODooKFD6cRWVUsX5yGjQ&usqp=CAU' }}
+          />
+          <TouchableOpacity onPress={() => navigation.navigate('EditProfiles')}>
+            <Feather style={{ marginLeft: 120, bottom: 20 }} name='edit' size={20} color='#32ff7e' />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.profileName}>
+          <Text style={styles.textName}>{user?.name}</Text>
+        </View>
+        <View style={styles.profileEmail}>
+          <Text style={styles.email}>{user?.email}</Text>
+        </View>
+      </View>
+
+      <View style={styles.componentInfo}>
+        <TouchableOpacity style={styles.componentAccount}>
+          <Entypo style={{ alignSelf: 'center' }} name='user' size={25} color={'#7052ff'} />
+          <Text style={{ color: '#fff', fontSize: 20, alignSelf: 'center', marginRight: 130 }}>Account</Text>
+          <MaterialIcons style={{ alignSelf: 'center' }} name='navigate-next' size={30} color='#fff' />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.componentAccount}>
+          <Feather style={{ alignSelf: 'center' }} name='settings' size={25} color={'#7052ff'} />
+          <Text style={{ color: '#fff', fontSize: 20, alignSelf: 'center', marginRight: 130 }}>Settings</Text>
+          <MaterialIcons style={{ alignSelf: 'center' }} name='navigate-next' size={30} color='#fff' />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.componentAccount}>
+          <Entypo style={{ alignSelf: 'center' }} name='users' size={25} color={'#7052ff'} />
+          <Text style={{ color: '#fff', fontSize: 20, alignSelf: 'center', marginRight: 130 }}>About Us</Text>
+          <MaterialIcons style={{ alignSelf: 'center' }} name='navigate-next' size={30} color='#fff' />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.fcUpdate}>
+        <TouchableOpacity style={{ flexDirection: 'row', alignSelf: 'center' }}>
+          <Text style={styles.textUpdate}>Sign out</Text>
+          <FontAwesome style={{ alignSelf: 'center', marginLeft: 10 }} name='sign-out' size={30} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -99,5 +156,88 @@ const Profile = ({navigation}: any) => {
 export default Profile;
 
 const styles = StyleSheet.create({
-  
+  profileContainer: {
+    flex: 1,
+    backgroundColor: '#333',
+  },
+  iconBack: {
+    marginHorizontal: 20,
+    marginTop: 20,
+    borderRadius: 15,
+  },
+  headerProfile: {
+    width: 200,
+    alignSelf: 'center',
+    position: 'absolute',
+    marginVertical: 30
+  },
+  profileText: {
+    color: "#fff",
+    fontSize: 18,
+    textAlign: 'center'
+  },
+  profileImage: {
+    marginVertical: 20,
+  },
+  userProfileImage: {
+    width: 120,
+    height: 120,
+    borderRadius: 100,
+    alignSelf: 'center'
+  },
+  profileName: {
+    alignItems: 'center'
+  },
+  profileEmail: {
+    alignItems: 'center',
+    marginVertical: 5
+  },
+  email: {
+    color: '#C3C7C7'
+  },
+  textName: {
+    color: '#fff',
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  componentInfo: {
+    top: 260,
+  },
+  styleUser: {
+    borderRadius: 100,
+    width: 100,
+    alignSelf: 'center',
+    // marginLeft: 60
+  },
+  componentAccount: {
+    backgroundColor: '#3E3D3D',
+    flexDirection: 'row',
+    borderRadius: 15,
+    width: "85%",
+    height: 60,
+    shadowColor: 'rgba(0, 0, 0, 0.5)',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 4,
+    elevation: 4,
+    marginVertical: 10,
+    justifyContent: 'space-evenly',
+    marginHorizontal: 30,
+  },
+  fcUpdate: {
+    top: 300,
+    borderRadius: 25,
+    alignSelf: 'center',
+    backgroundColor: '#fff',
+    width: '85%',
+  },
+  textUpdate: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginVertical: 15,
+    textAlign: 'center',
+  }
 });
